e3a38ed04461e05d42b70f625cc9652adacb987a
Laura Shapiro <laurashapiro01@gmail.com>
2018-08-30 16:19:59 -0400

    minor

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 var path = require('path')
+
 module.exports = {
 	configureWebpack: {
 		output: {
 	       path: path.join(__dirname, 'dist'),
 	       filename: 'bundle.js',
 	       globalObject: 'this'
-	},
+		},
 
 	}
 }
