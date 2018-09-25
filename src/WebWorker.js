dc7feaa5b2b12011265177ace26a82e13eae04a8
Laura Shapiro <laurashapiro01@gmail.com>
2018-09-25 13:27:21 -0400

    slightly revised fibonacci functionality

--- src/WebWorker.js
+++ src/WebWorker.js
@@ -1,26 +1,28 @@
 export function fibonacci(number) {
-
- 	let newNum = fibo(number-1)
- 	console.log(newNum)
- 	console.log(saveSequence)
- 	if (number === 0) {
- 		saveSequence = [0]
- 	}
-    return [newNum, saveSequence]
+    
+	let newNum = fibo(number)
+	let saver
+	if (number == 0 ) {
+	 	saver = [0]
+	} else if (number >= 1){
+	 	saver = saveSequence
+	}
+	return [newNum, saver]
 }
 
 let saveSequence = [0, 1]
 
 function fibo(n, sequence = [0, 1]){
-	sequence = sequence
+    sequence = sequence
 
-	if (n <= 1) {
-		return 1
-	}
+    if (n <= 1) {
+        return n
+    }
 
-	if (!sequence[n]) {
-		sequence[n] = fibo(n-1, sequence) + fibo(n-2, sequence)
-	}
-	saveSequence = sequence
-	return sequence[n]
+    if (!sequence[n]) {
+        sequence[n] = fibo(n-1, sequence) + fibo(n-2, sequence)
+    }
+    
+    saveSequence = sequence
+    return sequence[n]
 }
